9e786d88b7b513233e1c6d6fc82e58f48c50e068
unknown <Demo@1318-008-22.mck-ktits.ru>
2023-02-01 18:18:30 +0300

    new 01.02

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,12 @@
 import { NavLink } from "react-router-dom";
+import logo from "../../assets/logo.png";
 
 const CardButton = () => {
     return (
-        <button>Корзина: <span>1205 руб.</span></button>
+        <button>
+            <i class="fa-solid fa-cart-shopping">
+            </i><span>1205 руб.</span>
+        </button>
     )
 }
 
@@ -10,13 +14,18 @@ const Header = () => {
     return (
         <header className="header">
             <div className="logo">
+                <img src={logo} alt="logo" />
 
+                <div>
+                    <h1>React Sneakers</h1>
+                    <p>Магазин лучших кроссовок</p>
+                </div>
             </div>
 
             <nav className="nav">
                 <CardButton />
-                <NavLink to="/favorites">Избранное</NavLink>
-                <NavLink to="/orders">Заказы</NavLink>
+                <NavLink to="/favorites"><i class="fa-solid fa-heart"></i></NavLink>
+                <NavLink to="/orders"><i class="fa-solid fa-user"></i></NavLink>
 
             </nav>
         </header>
---
+++ src/components/Slider/Slide/Slide.jsx
@@ -0,0 +1,28 @@
+import { NavLink } from "react-router-dom";
+import slide from "../../../assets/slide.png";
+
+const Slide = () => {
+    return ( 
+        <div style={{
+            backgroundColor: "#F4EFE9"
+        }} className="slide">
+
+            <div className="part">
+
+                <h2>
+                    Stan Smith,<br />Forever!
+                </h2>
+
+                <NavLink to="/">
+                    Купить
+                </NavLink>
+
+            </div>
+
+            <img src={slide} alt="Slide" />
+
+        </div>
+    );
+}
+
+export default Slide;
