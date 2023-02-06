4d3018038b44d48a4ea70a21f83f7e5c267b006a
unknown <Demo@1302-008-22.mck-ktits.ru>
2023-02-06 19:46:05 +0300

    new 06.02.2023

--- src/data/products.js
+++ src/data/products.js
@@ -17,55 +17,55 @@ const PRODUCTS = [
         preview: product_1
     },
     {
-        id: 1,
+        id: 2,
         title: "Мужские Кроссовки Nike Air Max 270",
         price: "12999",
         preview: product_2
     },
     {
-        id: 1,
+        id: 3,
         title: "Мужские Кроссовки Nike Blazer Mid Suede",
         price: "12999",
         preview: product_3
     },
     {
-        id: 1,
+        id: 4,
         title: "Кроссовки Puma X Aka Boku Future Rider",
         price: "12999",
         preview: product_4
     },
     {
-        id: 1,
+        id: 5,
         title: "Мужские Кроссовки Under Armour Curry 8",
         price: "12999",
         preview: product_5
     },
     {
-        id: 1,
+        id: 6,
         title: "Мужские Кроссовки Nike Kyrie 7",
         price: "12999",
         preview: product_6
     },
     {
-        id: 1,
+        id: 7,
         title: "Мужские Кроссовки Jordan Air Jordan 11",
         price: "12999",
         preview: product_7
     },
     {
-        id: 1,
+        id: 8,
         title: "Мужские Кроссовки Nike LeBron XVIII",
         price: "12999",
         preview: product_8
     },
     {
-        id: 1,
+        id: 9,
         title: "Мужские Кроссовки Nike Lebron XVIII Low",
         price: "12999",
         preview: product_9
     },
     {
-        id: 1,
+        id: 10,
         title: "Мужские Кроссовки Nike Kyrie Flytrap IV",
         price: "12999",
         preview: product_10
